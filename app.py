0eb811f63649cb111d127f2508cb0a7694436c70
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-03-07 14:39:43 -0300

    Try - Except

--- app.py
+++ app.py
@@ -22,23 +22,37 @@ def exibir_opcoes():
     print('3. Cadastrar Restaurante')
     print('4. Sair\n')
 
+def opcao_invalida():
+    print('Opcão invalida!')
+    input('Digite uma tecla para voltar ao menu principal: ')
+    main()
+
+
+
 
 
 def escolher_opcao():
-    opcao = int(input('Escolha uma opção: '))
-    print(f'Você escolheu a opção {opcao}')
+    try:
+        opcao = int(input('Escolha uma opção: '))
+        print(f'Você escolheu a opção {opcao}')
 
 
-    if opcao == 1:
-        print('1. Cadastrar Restaurante')
-    elif opcao == 2:
-        print('2. Listar Restaurante')
-    elif opcao == 3:
-        print('3. Cadastrar Restaurante')
-    else:
-        finalizar_app()
+        if opcao == 1:
+            print('1. Cadastrar Restaurante')
+        elif opcao == 2:
+            print('2. Listar Restaurante')
+        elif opcao == 3:
+            print('3. Cadastrar Restaurante')
+        elif opcao == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
