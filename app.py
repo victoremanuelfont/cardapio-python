781b16aa58d2e0e104f87db23e2d08b513db2049
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-03-07 14:25:55 -0300

    Definição da Main como pagina inicial e outras funções

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 
-print("""
+
+def exibir_nome_do_programa():  
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -12,26 +14,38 @@ print("""
 def finalizar_app():
     os.system('cls')
     print('Encerrando o programa')
-    
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')
-print('3. Cadastrar Restaurante')
-print('4. Sair')
 
 
-opcao = int(input('Escolha uma opção: '))
-print(f'Você escolheu a opção {opcao}')
-
-
-if opcao == 1:
+def exibir_opcoes():
     print('1. Cadastrar Restaurante')
-elif opcao == 2:
     print('2. Listar Restaurante')
-elif opcao == 3:
     print('3. Cadastrar Restaurante')
-else:
-    finalizar_app()
+    print('4. Sair\n')
 
+
+
+def escolher_opcao():
+    opcao = int(input('Escolha uma opção: '))
+    print(f'Você escolheu a opção {opcao}')
+
+
+    if opcao == 1:
+        print('1. Cadastrar Restaurante')
+    elif opcao == 2:
+        print('2. Listar Restaurante')
+    elif opcao == 3:
+        print('3. Cadastrar Restaurante')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+if __name__ == '__main__':
+    main()
 
     
 
